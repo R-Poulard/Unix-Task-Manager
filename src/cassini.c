281b12e998e9f9b8ac985f3015caada3daf6b9e1
JEAN MARIE roude <roude123@hotmail.fr>
2021-11-21 18:14:11 +0100

    Update cassini.c

--- src/cassini.c
+++ src/cassini.c
@@ -46,10 +46,11 @@ int main(int argc, char * argv[]) {
   char * minutes_str = "*";
   char * hours_str = "*";
   char * daysofweek_str = "*";
-  char * pipes_directory = "/home/remi/Desktop/sy5-projet-2021-2022/run/pipes/";
+  char * pipes_directory = "/home/emma/Documents/Cours/2021/SYS5/sy5-projet-2021-2022/run/pipes/";
   
   uint16_t operation = CLIENT_REQUEST_LIST_TASKS;
   uint64_t taskid;
+  uint16_t conv;
   
   int opt;
   char * strtoull_endp;
@@ -57,14 +58,14 @@ int main(int argc, char * argv[]) {
   //VARIABLES
   char * fichier_test = "/tmp/fichier.txt";
   char * fifo_request = "saturnd-request-pipe";
-  char * fifo_reply = "saturnd-reply-pipe";
+  char * fifo_reply = "/saturnd-reply-pipe";
   
   char *str_request = malloc(strlen(pipes_directory)+strlen(fifo_request)+1);
   str_request[strlen(pipes_directory)+strlen(fifo_request)]='\0';
   strcat(str_request,pipes_directory);
   strcat(str_request,fifo_request);
   
-  int fd=open(fichier_test/*str_request*/, O_CREAT | O_WRONLY);
+  int fd=open(str_request, O_CREAT | O_WRONLY);
 	if(fd<0){
 	perror("open");
 	exit(EXIT_FAILURE);
@@ -74,8 +75,8 @@ int main(int argc, char * argv[]) {
   void *buf;
   void *buf3;
   uint16_t bigE;
-  commandline *command;
-  string *tab;
+  commandline command;
+  string  * tab ;
   int t;//timing from strings value
   int i;//indice
   char *pointeur;
@@ -138,73 +139,78 @@ int main(int argc, char * argv[]) {
   // --------
 
   //SWITCH
+  conv = htobe16(operation);
   switch(operation){
   case (CLIENT_REQUEST_LIST_TASKS)://'LS' *
     if(fd<0){
        exit(EXIT_FAILURE);
     }
-    if(write(fd,&operation,sizeof(uint16_t))<0){
+    if(write(fd,&conv,sizeof(uint16_t))<0){
        exit(EXIT_FAILURE);
     }
     break;
   case (CLIENT_REQUEST_CREATE_TASK)://'CR'
+	 if(write(fd,&conv,sizeof(uint16_t))<0){
+       exit(EXIT_FAILURE);
+    }
     time=malloc(sizeof(struct timing));//struct timing
     if(time==NULL)exit(EXIT_FAILURE);
     
     t=timing_from_strings(time,minutes_str,hours_str,daysofweek_str);
     if(t<0)exit(EXIT_FAILURE);
-    if(write(fd,time,sizeof(struct timing))<0){
+	uint64_t conv1 = htobe64(time->minutes);
+	uint32_t conv2 = htobe32(time->hours);
+
+    if(write(fd,&conv1,sizeof(uint64_t))<0){
        exit(EXIT_FAILURE);
-    }      
+    } 
+    if(write(fd,&conv2,sizeof(uint32_t))<0){
+       exit(EXIT_FAILURE);
+    } 
+    if(write(fd,&(time->daysofweek),sizeof(uint8_t))<0){
+       exit(EXIT_FAILURE);
+    }    
+
     
-    command=malloc(sizeof(commandline));//command
-    if(command==NULL) exit(EXIT_FAILURE);
+	//command=malloc(sizeof(commandline));//command
+    //if(command==NULL) exit(EXIT_FAILURE);
     
-    i=0;//In case -m -h -d option are defined
+    i=1;//In case -m -h -d option are defined
     if(strcmp(minutes_str,"*")!=0)i+=2;
     if(strcmp(hours_str,"*")!=0)i+=2;
     if(strcmp(daysofweek_str,"*")!=0)i+=2;
     
     if(argc-1<1) exit(EXIT_FAILURE);
-    command->ARGC=argc-i-2;
-    tab=malloc(sizeof(string)*(argc-i-1));
-    for(int j=0;j<argc-i-2;j++){
-      tab[j].L=strlen(argv[j+i+2]);
-      for(int a=0;a<tab[j].L;a++){
-      	tab[j].chaine[a]=argv[j+i+2][a];
-      }
-    }
-    /*printf("ARGC-i-2 = %d\n",argc-i-2);
-    for(int k=0;k<(argc-i-1);k++){
-      
-      printf("k=%d, %d\n",k,tab[k].L);
-      for(int m=0;m<tab[k].L;m++){
-	printf("%c",tab[k].chaine[m]);
-      }
-      if(tab[k].L==0)printf("NULL\n");
-      printf("\n");
-    }*/
-    command->ARGV=tab;
-
     
-    if(write(fd,command,sizeof(commandline))<0){
-		perror("write");
-      exit(EXIT_FAILURE);
+    uint32_t arg_con = htobe32(argc-i-3);
+    write(fd, &arg_con, sizeof(uint32_t));
+
+     for(int j=0;j<arg_con;j++){
+		uint32_t taille = strlen(argv[j+i+3]);
+		uint32_t taille2 = htobe32(taille);
+		write(fd,&taille2, sizeof(uint32_t));
+		//tab[j].L=taille;
+		for(int a=0;a<taille;a++){
+			//tab[j].chaine = malloc(tab[j].L);
+			//tab[j].chaine[a]=argv[j+i+3][a];
+			char k = argv[j+i+3][a];
+			//write(1,&k ,sizeof(1));
+			write(fd,&k ,1);	
+      }
     }
-    free(time);
-    free(command);
-    free(tab);
+    
+    
     break;
   case CLIENT_REQUEST_TERMINATE://'TM' *
     if(fd<0){
       exit(EXIT_FAILURE);
     }
-    if(write(fd,&operation,sizeof(uint16_t))<0){
+    if(write(fd,&conv,sizeof(uint16_t))<0){
       exit(EXIT_FAILURE);
     }
     break;
   case CLIENT_REQUEST_REMOVE_TASK://'RM' *
-    if(write(fd,&operation,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
+    if(write(fd,&conv,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
        exit(EXIT_FAILURE);
     }
     if(write(fd,&taskid,sizeof(uint64_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
@@ -212,7 +218,7 @@ int main(int argc, char * argv[]) {
     }
     break;
   case CLIENT_REQUEST_GET_TIMES_AND_EXITCODES://'TX' *
-     if(write(fd,&operation,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
+     if(write(fd,&conv,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
        exit(EXIT_FAILURE);
     }
     if(write(fd,&taskid,sizeof(uint64_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
@@ -220,7 +226,7 @@ int main(int argc, char * argv[]) {
     }
     break;
   case CLIENT_REQUEST_GET_STDOUT://'SO' *
-      if(write(fd,&operation,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
+      if(write(fd,&conv,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
        exit(EXIT_FAILURE);
     }
     if(write(fd,&taskid,sizeof(uint64_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
@@ -228,95 +234,128 @@ int main(int argc, char * argv[]) {
     }
     break;
   case CLIENT_REQUEST_GET_STDERR://'SE' *
-      if(write(fd,&operation,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
+      if(write(fd,&conv,sizeof(uint16_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
        exit(EXIT_FAILURE);
     }
     if(write(fd,&taskid,sizeof(uint64_t))<0){//uint64_t est converti dans fd (utiliser des valeurs > 33)
        exit(EXIT_FAILURE);
     }
+    break;
   }
 
  
-  char *str = malloc(strlen(pipes_directory)+strlen(fifo_reply)+1);
-  str[strlen(pipes_directory)+strlen(fifo_reply)]='\0';
+  //char *str = malloc(strlen(pipes_directory)+strlen(fifo_reply)+1);
+ // str[strlen(pipes_directory)+strlen(fifo_reply)]='\0';
   
   //Ajout du chemin vers le fichier dans str
-  strcat(str,pipes_directory);
-  strcat(str,fifo_reply);
-  
-  printf("%s\n",str);
-  int fd_reply=open(str,O_RDONLY);
-  printf("OPEN=%d\n",fd_reply);
+ // strcat(str,pipes_directory);
+ // strcat(str,fifo_reply);
+
+  int fd_reply=open("./run/pipes/saturnd-reply-pipe",O_RDONLY);
   struct timing *tmps;
   uint16_t *buf16;
   uint64_t *buf64;
   uint32_t *buf32;
+  
+      uint64_t buf64tmp;//=malloc(sizeof(uint64_t));
+    uint16_t buf16tmp;//=malloc(sizeof(uint16_t));
+    
   int rd;
   int wr;
-  uint16_t *er=SERVER_REPLY_ERROR;
-  
   switch(operation){
 	  
 	  case CLIENT_REQUEST_LIST_TASKS:
 	  buf16=malloc(sizeof(uint16_t));
 	  rd=read(fd_reply,buf16,sizeof(uint16_t));//REPTYPE
+	  if(rd<0){
+		  exit(1);
+	  }
 	  //wr=write(STDOUT_FILENO,buf2,sizeof(uint16_t));//OK
 	  //if(wr<0)goto error;
 	  
 	  buf32 = malloc(sizeof(uint32_t));
 	  rd=read(fd_reply,buf32,sizeof(uint32_t));//TASKID
-	  uint32_t nb=htobe32(buf32);	  
+	  if(rd<0){
+		  exit(1);
+	  }
+	  uint32_t nb=htobe32(*buf32);	  
 	  int i=0;
 	  while(i<nb){
 		  buf64=malloc(sizeof(uint64_t));
 		  rd=read(fd_reply,buf64,sizeof(uint64_t));
-		  write(STDOUT_FILENO,buf64,sizeof(uint64_t));
+		  if(rd<0){
+		  exit(1);
+	  }
+		  if(write(STDOUT_FILENO,buf64,sizeof(uint64_t))<0){
+			  exit(1);
+			  }
 		  free(buf64);
 		  //rajouter un espace
 		  tmps=malloc(sizeof(struct timing));
 		  rd=read(fd_reply,tmps,sizeof(struct timing));
+		  if(rd<0){
+		  exit(1);
+	  }
 		  char *str_buf3=malloc(sizeof(TIMING_TEXT_MIN_BUFFERSIZE));
 		  timing_string_from_timing(str_buf3,tmps);
-		  write(STDOUT_FILENO,str_buf3,TIMING_TEXT_MIN_BUFFERSIZE);
-		  write(STDOUT_FILENO," ",sizeof(char));
+		  if(write(STDOUT_FILENO,str_buf3,TIMING_TEXT_MIN_BUFFERSIZE)<0){
+		  exit(1);
+	  }
+		  if(write(STDOUT_FILENO," ",sizeof(char))<0){
+			  exit(1);
+		  }
 		  //rajouter un espace?
 		  free(tmps);
 		  free(str_buf3);
 		  
+		  
 		  commandline *buf4 = malloc(sizeof(commandline));
 		  rd=read(fd_reply,buf4,sizeof(commandline));
+		  if(rd<0){
+		  exit(1);
+	  }
 		  for(int i=0;i<buf4->ARGC;i++){
 			  for(int y=0;y<buf4->ARGV[i].L;y++){
-				 write(STDOUT_FILENO,(buf4->ARGV[i]).chaine[y],sizeof(char));
+				 if(write(STDOUT_FILENO,&(buf4->ARGV[i]).chaine[y],sizeof(char))<0){
+					 exit(1);
+					 }
 				}
-				write(STDOUT_FILENO," ",sizeof(char));
+				if(write(STDOUT_FILENO," ",sizeof(char))<0){
+					exit(1);
+				}
 		  }
 		  i++;
-		  write(STDOUT_FILENO,"\n",sizeof(char));
+		  if(write(STDOUT_FILENO,"\n",sizeof(char))<0){
+			  exit(1);
+		  }
 	  }
+	  
+	  //free(buf16);
+	  //free(buf64);
 	  break;
 	  
 	  
   case CLIENT_REQUEST_CREATE_TASK :
-    buf64=malloc(sizeof(uint64_t));
-    buf16=malloc(sizeof(uint16_t));
-    rd=read(fd_reply,buf16,sizeof(uint16_t));//REPTYPE   
-    rd=read(fd_reply,buf64,sizeof(uint64_t));//TASKID
-    wr=write(STDOUT_FILENO,buf64,sizeof(uint64_t));
-    free(buf64);
-    free(buf16);
+	
+	
+    rd=read(fd_reply,&buf16tmp,sizeof(uint16_t));//REPTYPE   
+    rd=read(fd_reply,&buf64tmp,sizeof(uint64_t));//TASKID
+    uint64_t ok = htobe64(buf64tmp);
+    wr=write(STDOUT_FILENO,&ok,sizeof(uint64_t));
+    
+    free(buf64tmp);
+    free(buf16tmp);
     break;
+    
   case CLIENT_REQUEST_REMOVE_TASK:
     buf16=malloc(sizeof(uint16_t));
     rd=read(fd_reply,buf16,sizeof(uint16_t));//REPTYPE
     //write(STDOUT_FILENO,buf2,sizeof(uint16_t));//
-    uint16_t *test=SERVER_REPLY_ERROR;
     uint16_t hto_buf=htobe16(buf16);
-    if(test==hto_buf){
+    if(SERVER_REPLY_ERROR==hto_buf){
       rd=read(fd_reply,&hto_buf,sizeof(uint16_t));//ERRCODE
       write(STDOUT_FILENO,&hto_buf,sizeof(uint16_t));
     }
-    free(test);
     free(buf16);
     break;
   case CLIENT_REQUEST_GET_STDOUT:
@@ -324,14 +363,14 @@ int main(int argc, char * argv[]) {
     rd=read(fd_reply,buf16,sizeof(uint16_t));//REPTYPE
     uint16_t hto_buf2=htobe16(buf16);
     string *str_buf=malloc(sizeof(struct string));;
-    if(er==hto_buf2){
+    if(SERVER_REPLY_ERROR==hto_buf2){
       rd=read(fd_reply,&hto_buf2,sizeof(uint16_t));//ERRCODE
       write(STDOUT_FILENO,&hto_buf2,sizeof(uint16_t));
     } 
     else {
       rd=read(fd_reply,str_buf,sizeof(string));//<string>
       for(int i=0;i<str_buf->L;i++){
-		write(STDOUT_FILENO,str_buf->chaine[i],sizeof(char));
+		write(STDOUT_FILENO,&str_buf->chaine[i],sizeof(char));
       }
       printf("\n");
     }
@@ -345,14 +384,14 @@ int main(int argc, char * argv[]) {
     
     uint16_t hto_buf3=htobe16(buf16);
     string *str_buf2=malloc(sizeof(struct string));
-    if(er==hto_buf3){
+    if(SERVER_REPLY_ERROR==hto_buf3){
       rd=read(fd_reply,&hto_buf3,sizeof(uint16_t));//ERRCODE
       write(STDOUT_FILENO,&hto_buf3,sizeof(uint16_t));
     } 
     else {
       rd=read(fd_reply,&str_buf2,sizeof(string));//<string>
       for(int i=0;i<str_buf2->L;i++){
-		write(STDOUT_FILENO,str_buf2->chaine[i],sizeof(char));
+		write(STDOUT_FILENO,&str_buf2->chaine[i],sizeof(char));
       }
       printf("\n");
     }
@@ -376,4 +415,3 @@ int main(int argc, char * argv[]) {
   pipes_directory = NULL;
   return EXIT_FAILURE;
 }
-
