75bad5d9ebd6a5bd07140dc4132a1caa585d9cc7
JEAN MARIE roude <roude123@hotmail.fr>
2021-12-28 13:47:43 +0100

    Update saturnd.c

--- src/saturnd.c
+++ src/saturnd.c
@@ -11,6 +11,59 @@
 #include <endian.h>
 #include <time.h>
 #include <dirent.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int parsec (char *global,char* subside,char del){
+     int last_one=-1;
+     int i=0;
+     while(global[i]!='\0'){
+	if(global[i]==del){
+        last_one=i;
+        }
+        i++;    
+     }
+     if (last_one==-1){
+        exit(1);
+     }
+     else{
+       subside=malloc(sizeof(char)*strlen(global+last_one+1));
+       strncpy(subside,global+last_one+1,strlen(global+last_one+1));
+       global[last_one]='\0';
+       exit(0);
+     }
+}
+
+int compaire_cron(int NOSMINUTES,char* minutes){
+    minutes;
+     char *current;
+     while(parsec(minutes,current,',')!=0){
+       char* siunder;      
+       if(parsec(current,siunder,'-')!=0){
+          if(atoi(current)==NOSMINUTES){
+             free(current);
+             free(minutes);
+             return 1;
+          }
+       }
+       else{
+        int petit =atoi(current);
+        int grand=atoi(siunder);
+        if(petit<=NOSMINUTES && NOSMINUTES <= grand){
+           free(current);
+           free(minutes);
+           free(siunder);
+           return 1;
+         }
+       }
+    }
+    return 0;
+}
+
+//if(compaire_cron(minutes,cron_min) && compaire_cron(heures,cron_heur) && compaire_cron(day,cron_day)){}//A METTRE DANS LE CODE
+
+
+
 
 int main(){
   char * pipes_directory;
@@ -33,14 +86,14 @@ int main(){
   int fd_reply = mkfifo(fifo_reply, S_IRWXU);
   
   int pip[2];
-  pipe(pip,O_NONBLOCK);
+  pipe(pip);
   
   int taskmax=open("taskid_max",O_CREAT | O_EXCL | O_RDWR );
-  if(taskmax<0 && errno!=EEXIST)goto;
+  //if(taskmax<0 && errno!=EEXIST)goto;
   
   pid_t pid = fork();
-  if(pid<0)goto;
-  if(pid>0){//Père
+  //if(pid<0)goto;
+  if(pid>0){//PÃ¨re
   
   
   
@@ -53,18 +106,28 @@ int main(){
   else
   {
   //Fils
-  char * pathtasks = malloc(256)//MALLOC
+  char * pathtasks = malloc(256);//MALLOC
   strcat(pathtasks,"/tmp/");
   strcat(pathtasks,username_str);
   strcat(pathtasks,"/saturnd/tasks");
   
-  mkdir(pathtasks,O_CREAT | O_EXCL | O_RDWR);)
+  mkdir(pathtasks,O_CREAT | O_EXCL | O_RDWR);
   
   DIR * dir=opendir(pathtasks);
-  struct * dirent struct_dir;
+  struct dirent * struct_dir;
   
-  while(struct_dir=readdir(dir)){
-    DIR * taskdir = opendir(struct_dir->d_name,O);
+  time_t time2 = time(NULL);
+  uint16_t code;
+  while(read(pip[0],&code,sizeof(uint16_t))>0){
+  
+  pid_t pid2=fork();
+  
+  if(pid2>0){
+    sleep(60);
+    time_t time3=time(NULL);
+  } else {
+  while((struct_dir=readdir(dir))){
+    DIR * taskdir = opendir(struct_dir->d_name);
     char * pathtime = malloc(256);//MALLOC
     strcat(pathtime,pathtasks);
     strcat(pathtime,"/time_exec");
@@ -74,9 +137,9 @@ int main(){
     uint32_t heures;
     uint8_t jours;
     
-    int rd=read(fd_time,&minutes,sizeof(uint64_t))//READ
-    rd=read(fd_time,&heures,sizeof(uint32_t))//READ
-    rd=read(fd_time,&jours,sizeof(uint8_t))//READ
+    int rd=read(fd_time,&minutes,sizeof(uint64_t));//READ
+    rd=read(fd_time,&heures,sizeof(uint32_t));//READ
+    rd=read(fd_time,&jours,sizeof(uint8_t));//READ
     
     char * buf_minutes=malloc(TIMING_TEXT_MIN_BUFFERSIZE);//MALLOC
     char * buf_heures=malloc(TIMING_TEXT_MIN_BUFFERSIZE);//MALLOC
@@ -102,7 +165,7 @@ int main(){
           int y=0;
           while(buf_minutes[i]!='\0'){
     	if(buf_minutes[i]=','){
-    	  s
+    	  
     	}
     	else {
     	buf_avant[y]=buf_minutes[i];
@@ -131,31 +194,58 @@ int main(){
         uint32_t * buf32;
         char ** cmd_argv;
       
-        int rd=read(fd_cmd,&cmd_argc,sizeof(uint32_t))//READ
+        int rd=read(fd_cmd,&cmd_argc,sizeof(uint32_t));//READ
         cmd_argc=htobe32(cmd_argc);
-        cmd_argv(cmd_argc*sizeof(char *));
+        cmd_argv=malloc(cmd_argc*sizeof(char *));
         int x=0;
       
         rd=read(fd_cmd,buf32,sizeof(uint32_t));//READ
-        cmd=malloc(sizeof(char)*buf32+1);
+        cmd=malloc(buf32+1);
         
         rd=read(fd_cmd,cmd,buf32);//READ
-        cmd[buf32]='\0';
+        cmd[*(buf32)]='\0';
       
         for(int i=0;i<cmd_argc-1;i++){
           rd=read(fd_cmd,buf32,sizeof(uint32_t));//READ
-          cmd_argv[i]=malloc(sizeof(char)*buf32+1);
+          cmd_argv[i]=malloc(buf32+1);
         
           rd=read(fd_cmd,cmd_argv[i],buf32);//READ
-          cmd_argv[buf32]='\0';
+          cmd_argv[*(buf32)]='\0';
         }
         cmd_argv[cmd_argc-1]=NULL;
         close(fd_cmd);
-        execvp(cmd,cmd_argv);
+        
+        char * cmd_out = malloc(50);//MALLOC
+        strcat(cmd_out,pathtasks);
+        strcat(cmd_out,"stdout");
+        
+        char * cmd_err = malloc(50);//MALLOC
+        strcat(cmd_err,pathtasks);
+        strcat(cmd_err,"error");
+        
+        char * cmd_time = malloc(50);//MALLOC
+        strcat(cmd_time,pathtasks);
+        strcat(cmd_time,"time");
+        
+        int fd_out=open(cmd_out,O_WRONLY);//OPEN
+        int fd_err=open(cmd_err,O_WRONLY);//OPEN
+        int fd_time=open(cmd_time,O_WRONLY);//OPEN
+        
+        write(fd_time,&time2,sizeof(int64_t));//WRITE
+        dup2(fd_out,STDOUT_FILENO);//DUP
+        dup2(fd_err,STDERR_FILENO);//DUP
+        
+        execvp(cmd,cmd_argv);//EXEC
         } else {
-          waitpid(pid);
+          wait(NULL);
         }
      }
+     }
   }
+  }
+  while (wait(NULL) > 0);
+  closedir(dir);
+  free(pathtasks);
+  exit(EXIT_SUCCESS);
   }
 }
